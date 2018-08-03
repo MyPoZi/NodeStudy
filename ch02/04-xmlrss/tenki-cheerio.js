dc389da4e12ec95c9d04d0f7e7a1357910d2f68b
MyPoZi <kxx555u.u@gmail.com>
2018-08-03 22:08:11 +0900

    少し訂正

--- ch02/04-xmlrss/tenki-cheerio.js
+++ ch02/04-xmlrss/tenki-cheerio.js
@@ -1,11 +1,30 @@
-var RSS = "https://rss-weather.yahoo.co.jp/rss/days/5610.xml";
+var RSS = "https://rss-weather.yahoo.co.jp/rss/days/4410.xml";
 
 var client = require("cheerio-httpcli");
+var request = require('request');
 
-client.fetch(RSS,function (err,$,res) {
-    if (err){ console.log(err); return;}
+client.fetch(RSS, function (err, $, res) {
+    if (err) {
+        console.log(err);
+        return;
+    }
     $("item > title").each(function () {
         var title = $(this).text();
-        console.log(title);
-    })
+        var options = {
+            uri: 'https://hooks.slack.com/services/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',
+            headers: {'Content-Type': 'application/json'},
+            json: {
+                username: '天気サーバー',
+                icon_emoji: ':sunny:',
+                text: title
+            }
+        };
+        request.post(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                console.log(body);
+            } else {
+                console.log('error: ' + response.statusCode + '\n' + response.body);
+            }
+        });
+    });
 });
